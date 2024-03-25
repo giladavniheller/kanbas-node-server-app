17f984c8cbe0f50ee53845f5d2c2f0098f21f5f8
giladavniheller <57957977+giladavniheller@users.noreply.github.com>
2024-03-25 15:52:09 -0400

    first commit

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,510 @@
+export default [
+  {
+    _id: "M101",
+    name: "Introduction to Rocket Propulsion",
+    description: "Basic principles of rocket propulsion and rocket engines.",
+    course: "CS4550",
+    lessons: [
+      {
+        _id: "L101",
+        name: "History of Rocketry",
+        description: "A brief history of rocketry and space exploration.",
+        module: "CS4550",
+      },
+      {
+        _id: "L102",
+        name: "Rocket Propulsion Fundamentals",
+        description: "Basic principles of rocket propulsion.",
+        module: "CS4550",
+      },
+      {
+        _id: "L103",
+        name: "Rocket Engine Types",
+        description: "Overview of different types of rocket engines.",
+        module: "CS4550",
+      },
+    ],
+  },
+  {
+    _id: "M102",
+    name: "Fuel and Combustion",
+    description:
+      "Understanding rocket fuel, combustion processes, and efficiency.",
+    course: "CS4550",
+    lessons: [
+      {
+        _id: "L201",
+        name: "Rocket Fuel",
+        description: "Overview of different types of rocket fuels.",
+        module: "CS4550",
+      },
+      {
+        _id: "L202",
+        name: "Combustion Processes",
+        description: "Understanding combustion processes and efficiency.",
+        module: "CS4550",
+      },
+      {
+        _id: "L203",
+        name: "Combustion Instability",
+        description: "Understanding combustion instability and mitigation.",
+        module: "CS4550",
+      },
+    ],
+  },
+  {
+    _id: "M103",
+    name: "Nozzle Design",
+    description:
+      "Principles of rocket nozzle design and performance optimization.",
+    course: "CS4550",
+    lessons: [
+      {
+        _id: "L301",
+        name: "Nozzle Design",
+        description: "Overview of different types of rocket nozzles.",
+        module: "CS4550",
+      },
+      {
+        _id: "L302",
+        name: "Nozzle Performance",
+        description: "Understanding nozzle performance and efficiency.",
+        module: "CS4550",
+      },
+      {
+        _id: "L303",
+        name: "Nozzle Optimization",
+        description: "Optimizing nozzle design for specific applications.",
+        module: "CS4550",
+      },
+    ],
+  },
+  {
+    _id: "M201",
+    name: "Getting Good",
+    description: "Basic concepts surrounding greatness, coolness, etc.",
+    course: "CS1234",
+    lessons: [
+      {
+        _id: "L201_1",
+        name: "Getting alright",
+        description: "Beginner level goodness, need to chill out.",
+        module: "CS1234",
+      },
+      {
+        _id: "L201_2",
+        name: "Getting pretty ok",
+        description: "Intermediate chillin'.",
+        module: "CS1234",
+      },
+      {
+        _id: "L201_3",
+        name: "Finishing up",
+        description: "You have mastered the art of the chiller. Now relax.",
+        module: "CS1234",
+      },
+    ],
+  },
+  {
+    _id: "M202",
+    name: "Categories of Modern Life",
+    description: "Understanding categorical data in regard to everyday living.",
+    course: "CS1234",
+    lessons: [
+      {
+        _id: "L202_1",
+        name: "Politics & Prose",
+        description: "Overview of different types of politics and prose.",
+        module: "CS1234",
+      },
+      {
+        _id: "L202_2",
+        name: "Everything else",
+        description: "Understanding everything in all other categories.",
+        module: "CS1234",
+      },
+    ],
+  },
+  {
+    _id: "M203",
+    name: "Good Bagel Combinations",
+    description: "Study of bagels and accompanying foodstuffs.",
+    course: "CS1234",
+    lessons: [
+      {
+        _id: "L203_1",
+        name: "Sesame bagel & garlic and chive cream cheese",
+        description:
+          "An introductory investigation into Pavement Coffee and their bagel options.",
+        module: "CS1234",
+      },
+      {
+        _id: "L203_2",
+        name: "Everything bagel with normal cream cheese",
+        description:
+          "A modern day classic, not too flashy but always dependable.",
+        module: "CS1234",
+      },
+      {
+        _id: "L203_3",
+        name: "Anything as long as there's lox",
+        description:
+          "Understanding the fact that sometimes you have to take what you're given.",
+        module: "CS1234",
+      },
+    ],
+  },
+  {
+    _id: "M301",
+    name: "Vegetables In Kellan's Fridge",
+    description: "What's in Kellan's Fridge? Let's find out.",
+    course: "ARTF1124",
+    lessons: [
+      {
+        _id: "L301",
+        name: "Four Carrots",
+        description: "Ooh! More than three but definitely fewer than five.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L302",
+        name: "Three Tomatos",
+        description:
+          "Tomato tomato, kinda need to say it out loud to hear the difference.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L303",
+        name: "A Head of Lettuce",
+        description: "Not a shoulder, knee, or toe.",
+        module: "ARTF1124",
+      },
+    ],
+  },
+  {
+    _id: "M302",
+    name: "Animals Kellan Could Fight",
+    description:
+      "Understanding which creature Kellan could best in a bare knuckle brawl.",
+    course: "ARTF1124",
+    lessons: [
+      {
+        _id: "L301",
+        name: "Colony of Ants (maybe)",
+        description:
+          "Individually a breeze, together a force to be reckoned with.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L302",
+        name: "Grizzly Bear",
+        description: "Easy peasy lemon squeezy.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L303",
+        name: "Every Pokemon",
+        description: "Learn why many experts are skeptical.",
+        module: "ARTF1124",
+      },
+    ],
+  },
+  {
+    _id: "M999",
+    name: "Life is Like a",
+    description: "... I guess we'll find out on this one.",
+    course: "ARTF1124",
+    lessons: [
+      {
+        _id: "L999_1",
+        name: "Box of Chocolates",
+        description: "Forrest Gump would agree.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L999_2",
+        name: "Lemonade Stand (You have the lemons)",
+        description: "Lemonade Mouth stans have been saying this.",
+        module: "ARTF1124",
+      },
+      {
+        _id: "L999_3",
+        name: "Lemonade Stand (You do not yet have the lemons)",
+        description:
+          "This part is much trickier, kinda need the lemons for this one.",
+        module: "ARTF1124",
+      },
+    ],
+  },
+  {
+    _id: "M401",
+    name: "Excellency In Rhyming",
+    description: "Scooby Dooby Dippity Do.",
+    course: "ARTF1125",
+    lessons: [
+      {
+        _id: "L401_1",
+        name: "Loosey Goosey",
+        description: "Gotta make sure to tighten things real good.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L401_2",
+        name: "Walky Talky",
+        description: "Fun to say and fun to use.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L401_3",
+        name: "See ya' later aligator",
+        description: "In a while crocadile.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L401_4",
+        name: "The sun shines sometimes",
+        description: "Paul McCartney was definitely onto something.",
+        module: "ARTF1125",
+      },
+    ],
+  },
+  {
+    _id: "M402",
+    name: "Meals in the Day",
+    description: "Understanding when you should be eating.",
+    course: "ARTF1125",
+    lessons: [
+      {
+        _id: "L402_1",
+        name: "Breakfast",
+        description: "A timeless classic.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L402_2",
+        name: "Second Breakfast",
+        description: "Hobbits have been on this wave for quite some time.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "402_3",
+        name: "Midnight Snack",
+        description:
+          "A hidden gem, many do not realize you can eat this late at night.",
+        module: "ARTF1125",
+      },
+    ],
+  },
+  {
+    _id: "M403",
+    name: "Intersection of Colors and Fruits",
+    description:
+      "Two classic categories with some suprising intersectionalities.",
+    course: "ARTF1125",
+    lessons: [
+      {
+        _id: "L403_1",
+        name: "Starfruit",
+        description: "You heard it here first.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L403_2",
+        name: "Grape",
+        description: "Eat em' by the dozen for a reason.",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L403_3",
+        name: "Watermelon",
+        description: "At the beginning of time there were four elements...",
+        module: "ARTF1125",
+      },
+      {
+        _id: "L403_4",
+        name: "Banana",
+        description: "Bannana ramma, bannan phone, bananananananana.",
+        module: "ARTF1125",
+      },
+    ],
+  },
+  {
+    _id: "M501",
+    name: "Off Whites",
+    description: "Understanding colors and their nearby neighbors.",
+    course: "DS3000",
+    lessons: [
+      {
+        _id: "L501_1",
+        name: "Eggshell",
+        description: "Cracking me up fr.",
+        module: "DS3000",
+      },
+      {
+        _id: "L501_2",
+        name: "Bone",
+        description: "Oof ouch owie! Definitely don't wanna break these.",
+        module: "DS3000",
+      },
+      {
+        _id: "L501_3",
+        name: "Cream",
+        description: "Perfect in a cup of joe.",
+        module: "DS3000",
+      },
+    ],
+  },
+  {
+    _id: "M502",
+    name: "Extremeties of a black and white scale",
+    description: "Understanding either polar ends is very important.",
+    course: "DS3000",
+    lessons: [
+      {
+        _id: "L502_1",
+        name: "Black",
+        description: "Known from such things as Closing Your Eyes.",
+        module: "DS3000",
+      },
+      {
+        _id: "L502_2",
+        name: "White",
+        description:
+          "Known from such things as What You See At the End of the Tunnel.",
+        module: "DS3000",
+      },
+    ],
+  },
+  {
+    _id: "M503",
+    name: "Sweet Treats and Nice Things",
+    description: "Yummy Scrumptous Yup Yup Yup.",
+    course: "DS3000",
+    lessons: [
+      {
+        _id: "L503_1",
+        name: "Pastry",
+        description: "Overview of a little treat.",
+        module: "DS3000",
+      },
+      {
+        _id: "L503_2",
+        name: "Chocolate (Strawberry)",
+        description:
+          "Very good they sell them at college convenience on Huntington.",
+        module: "DS3000",
+      },
+      {
+        _id: "L503_3",
+        name: "Green Tea",
+        description: "Tasty sweet and even with a little caffein.",
+        module: "DS3000",
+      },
+    ],
+  },
+  {
+    _id: "M504",
+    name: "Bad Bad Really Bad Things",
+    description: "Understanding things you should avoid.",
+    course: "DS3000",
+    lessons: [
+      {
+        _id: "L504_1",
+        name: "War",
+        description: "Bad!.",
+        module: "DS3000",
+      },
+      {
+        _id: "L504_2",
+        name: "Stubbing my toe",
+        description: "Really bad!",
+        module: "DS3000",
+      },
+      {
+        _id: "L504_3",
+        name: "Taxes",
+        description: "Super bad!",
+        module: "DS3000",
+      },
+    ],
+  },
+  {
+    _id: "M601",
+    name: "Winkler Songs (Best Of)",
+    description: "Banger after Banger after Banger.",
+    course: "CS3000",
+    lessons: [
+      {
+        _id: "L601_1",
+        name: "Hillside Home",
+        description: "Like when you live on Hillside.",
+        module: "CS3000",
+      },
+      {
+        _id: "L601_2",
+        name: "Hindsight",
+        description:
+          "Like when you are thinking back on something with new knowledge.",
+        module: "CS3000",
+      },
+      {
+        _id: "L601_3",
+        name: "Evening Plans",
+        description: "Wanna hang out tonight?",
+        module: "CS3000",
+      },
+    ],
+  },
+  {
+    _id: "M699",
+    name: "Bob Dylan Songs (Best Of)",
+    description: "Robert Zimmerman: A Case Study.",
+    course: "CS3000",
+    lessons: [
+      {
+        _id: "L601_1",
+        name: "Piano Man",
+        description: "His first major hit.",
+        module: "CS3000",
+      },
+      {
+        _id: "L601_2",
+        name: "Piano Man (Reprise)",
+        description: "Gotta add a reprise.",
+        module: "CS3000",
+      },
+      {
+        _id: "L601_3",
+        name: "Munch",
+        description: "You think YOU'RE the fart???",
+        module: "CS3000",
+      },
+    ],
+  },
+  {
+    _id: "M602",
+    name: "Dates That Might be Life Changing Events",
+    description: "Make sure to check your calendar.",
+    course: "CS3000",
+    lessons: [
+      {
+        _id: "L602_1",
+        name: "Retirement",
+        description: "Can't wait fr fr.",
+        module: "CS3000",
+      },
+      {
+        _id: "L602_2",
+        name: "April 3rd, 2027",
+        description: "Stay attentivie (O.O)",
+        module: "CS3000",
+      },
+      {
+        _id: "L602_3",
+        name: "The Day of my Daughter's Wedding",
+        description: "You come to me to ask for a favor???",
+        module: "CS3000",
+      },
+    ],
+  },
+];
